73cfb781336cd9514247d0e8eace33f6252c1786
Sumant Chhunchha <mr.chhunchha@gmail.com>
2016-09-21 09:50:01 -0700

    Injection and service

---
+++ my_project/workshop-app/src/app/data.service.ts
@@ -0,0 +1,82 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class DataService {
+  
+  racket_brands = {
+    head: 'Head',
+    prince: 'Prince',
+    babolat: 'Babolat',
+    wilson: 'Wilson'
+  }
+
+  racket_images = {
+    head : "http://www.directtennis.co.uk/Images/ExtraLarge/head_ti_s6_tennis_racket_black.jpg",
+    wilson : "http://tennisbusters.com/wp-content/uploads/2014/12/wilson-tennis-rackets-9.jpg",
+    prince : "http://tennisbusters.com/wp-content/uploads/2014/12/prince-tennis-racket-10.jpg",
+    babolat : "http://i.ebayimg.com/00/s/ODAwWDgwMA==/z/2aYAAOxyRNJSgnl8/$_1.JPG?set_id=2"
+  }
+
+  rackets = {
+    data: [
+      {
+      "name": "Head A",
+      "weight": 100,
+      "img": this.racket_images.head,
+      "brand": this.racket_brands.head
+      },
+      {
+        "name": "Wilson A",
+        "weight": 90,
+        "img": this.racket_images.wilson,
+        "brand": this.racket_brands.wilson
+      },
+      {
+        "name": "Prince A",
+        "weight": 110,
+        "img": this.racket_images.prince,
+        "brand": this.racket_brands.prince
+      },
+      {
+        "name": "Babolat A",
+        "weight": 95,
+        "img": this.racket_images.babolat,
+        "brand": this.racket_brands.babolat
+      },
+      {
+        "name": "Head B",
+        "weight": 105,
+        "img": this.racket_images.head,
+        "brand": this.racket_brands.head
+      },
+      {
+        "name": "Wilson B",
+        "weight": 95,
+        "img": this.racket_images.wilson,
+        "brand": this.racket_brands.wilson
+      },
+      {
+        "name": "Prince B",
+        "weight": 112,
+         "img": this.racket_images.prince,
+         "brand": this.racket_brands.prince
+      },
+      {
+        "name": "Babolat B",
+        "weight": 90,
+        "img": this.racket_images.babolat,
+        "brand": this.racket_brands.babolat
+      }
+    ]
+  };
+
+  constructor() { }
+
+  getRackets() {
+    return this.rackets.data;
+  }
+
+  getRacketImages() {
+    return this.racket_images;
+  }
+}
--- my_project/workshop-app/src/app/selection-screen/selection-screen.component.ts
+++ my_project/workshop-app/src/app/selection-screen/selection-screen.component.ts
@@ -1,94 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { rackets, racket_images, Racket } from './../data.ts';
-
+import { racket_images, Racket } from './../data.ts';
+import { DataService } from './../data.service.ts';
 import * as _ from 'lodash';
-
-// let racket_images = {
-//   head : "http://www.directtennis.co.uk/Images/ExtraLarge/head_ti_s6_tennis_racket_black.jpg",
-//   wilson : "http://tennisbusters.com/wp-content/uploads/2014/12/wilson-tennis-rackets-9.jpg",
-//   prince : "http://tennisbusters.com/wp-content/uploads/2014/12/prince-tennis-racket-10.jpg",
-//   babolat : "http://i.ebayimg.com/00/s/ODAwWDgwMA==/z/2aYAAOxyRNJSgnl8/$_1.JPG?set_id=2"
-// }
-
-console.log(racket_images);
-
-// let racket_brands = {
-//   head: 'Head',
-//   prince: 'Prince',
-//   babolat: 'Babolat',
-//   wilson: 'Wilson'
-// }
- 
- //moved to data.ts
-// let rackets = {
-//     data: [
-//       {
-//       "name": "Head A",
-//       "weight": 100,
-//       "img": racket_images.head,
-//       "brand": racket_brands.head
-//       },
-//       {
-//         "name": "Wilson A",
-//         "weight": 90,
-//         "img": racket_images.wilson,
-//         "brand": racket_brands.wilson
-//       },
-//       {
-//         "name": "Prince A",
-//         "weight": 110,
-//         "img": racket_images.prince,
-//         "brand": racket_brands.prince
-//       },
-//       {
-//         "name": "Babolat A",
-//         "weight": 95,
-//         "img": racket_images.babolat,
-//         "brand": racket_brands.babolat
-//       },
-//       {
-//         "name": "Head B",
-//         "weight": 105,
-//         "img": racket_images.head,
-//         "brand": racket_brands.head
-//       },
-//       {
-//         "name": "Wilson B",
-//         "weight": 95,
-//         "img": racket_images.wilson,
-//         "brand": racket_brands.wilson
-//       },
-//       {
-//         "name": "Prince B",
-//         "weight": 112,
-//          "img": racket_images.prince,
-//          "brand": racket_brands.prince
-//       },
-//       {
-//         "name": "Babolat B",
-//         "weight": 90,
-//         "img": racket_images.babolat,
-//         "brand": racket_brands.babolat
-//       }
-//     ]
-// };
 
 @Component({
   selector: 'app-selection-screen',
   templateUrl: './selection-screen.component.html',
-  styleUrls: ['./selection-screen.component.css']
+  styleUrls: ['./selection-screen.component.css'],
+  providers: [DataService]
 })
 export class SelectionScreenComponent implements OnInit {
-  rackets = rackets.data;
-  unique_brands = _.sortBy(_.uniq(_.map(rackets.data, 'brand')));
-  unique_weights = _.sortBy(_.uniq(_.map(rackets.data, 'weight')));
+  rackets;
+  unique_brands;
+  unique_weights;
   selectedRackets: Racket[] = [];
   search: string;
   filters = {};
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.rackets = this.dataService.getRackets();
+    this.unique_brands = _.sortBy(_.uniq(_.map(this.rackets, 'brand')));
+    this.unique_weights = _.sortBy(_.uniq(_.map(this.rackets, 'weight')));
   }
 
   setSelectedRacket(racket: Racket) {
